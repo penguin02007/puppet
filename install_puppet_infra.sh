2198481fbd4a92c4f59d7ad3b9ddf476e7ccce95
Leo Chan <penguin02007@gmail.com>
2017-11-10 21:44:10 -0500

    fix ruby syntax

--- install_puppet_infra.sh
+++ install_puppet_infra.sh
@@ -6,9 +6,11 @@
 #  https://github.com/ppouliot/puppet-puppet_infrastructure
 #  https://puppet.com/docs/puppet/5.3/install_linux.html
 #
-curl https://apt.puppetlabs.com/puppet5-release-xenial.deb -o /tmp/puppet5-release-xenial.deb
+curl https://apt.puppetlabs.com/puppet5-release-xenial.deb -o /tmp/puppet5-release-xenial.deb 
 apt-get install /tmp/puppet5-release-xenial.deb
 apt-get update && apt-get install aptitude -y
+# curl https://yum.puppetlabs.com/puppet5/puppet5-release-el-7.noarch.rpm -o /tmp/puppet5-release-el-7.noarch.rpm
+# yum localinstall /tmp/puppet5-release-el-7.noarch.rpm
 apt-cache show puppet-agent|grep -i 'puppet\s5' >>/dev/null && apt-get install ruby puppet-agent -y
 gem install r10k hiera-eyaml
 cd /etc/puppetlabs/code && wget https://raw.githubusercontent.com/penguin02007/puppet-puppet_infrastructure/master/Puppetfile
